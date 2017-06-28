0332decaded8d20d8ebd6b9aed59d6538925369a
ksea55 <wangyang198118@126.com>
2017-06-28 14:57:55 +0800

    HQL与QCQ(Criteria)查询案列

--- src/main/java/com/ksea/hibernate/query/hql/HibernateHqlQuery.java
+++ src/main/java/com/ksea/hibernate/query/hql/HibernateHqlQuery.java
@@ -43,12 +43,151 @@ public class HibernateHqlQuery {
 
     @Test
     public void test1() {
+        //HQL 查询全部
         Transaction transaction = session.beginTransaction();
         List list = session.createQuery("from Student").list();
         System.out.println(list);
 
         List<Student> students = session.createQuery("from Student", Student.class).list();
         System.out.println(students);
+
+        transaction.commit();
+    }
+
+    @Test
+    public void test2() {
+        //HQL排序查询
+        Transaction transaction = session.beginTransaction();
+        List list = session.createQuery("from Student order by age desc").list();
+        list.forEach(System.out::println);
+
+        transaction.commit();
+    }
+
+
+    @Test
+    public void test3() {
+        //HQL动态绑定参数，根据条件查询数据
+        Transaction transaction = session.beginTransaction();
+
+        //第一种
+        String hql = "from Student where age>? and score >?";
+        List list = session.createQuery(hql)
+                .setInteger(0, 30)
+                .setInteger(1, 85).list();
+        list.forEach(System.out::println);
+
+        System.out.println("-------------------------------------------");
+        //第二种
+        hql = "from Student where age>:age and score >:score";
+        list = session.createQuery(hql)
+                .setInteger("age", 30)
+                .setInteger("score", 85).list();
+        list.forEach(System.out::println);
+
+
+        //第三种
+        hql = "from Student where age>:age and score >:score";
+        list = session.createQuery(hql)
+                .setParameter("age", 30)
+                .setParameter("score", 85).list();
+        list.forEach(System.out::println);
+
+
+        transaction.commit();
+    }
+
+    @Test
+    public void test4() {
+        //HQL分页查询
+        Transaction transaction = session.beginTransaction();
+        List list = session.createQuery("from Student")
+                .setFirstResult((2 - 1) * 5) //startIndex=(startIndex-1)*pageSize
+                .setMaxResults(5) //pageSize
+                .list();
+        list.forEach(System.out::println);
+
+        transaction.commit();
+    }
+
+
+    @Test
+    public void test5() {
+        //HQL 中的like模糊查询
+        Transaction transaction = session.beginTransaction();
+
+        String hql = "from Student where name like :name";
+        List list = session.createQuery(hql)
+                .setParameter("name", "%3%")
+                .list();
+        list.forEach(System.out::println);
+
+        transaction.commit();
+    }
+
+    @Test
+    public void test6() {
+        //HQL 根据id查询唯一数据
+        Transaction transaction = session.beginTransaction();
+
+        String hql = "from Student where id=:id";
+        Student student = session.createQuery(hql, Student.class)
+                .setParameter("id", 3)
+                .uniqueResult();
+        System.out.println(student);
+
+        transaction.commit();
+    }
+
+    @Test
+    public void test7() {
+        //HQL 聚合函数查询
+        Transaction transaction = session.beginTransaction();
+
+        String hql = "select count(*) from Student";
+        Long count = session.createQuery(hql, Long.class)
+                .uniqueResult();
+        System.out.println(count); //总条数:20
+
+
+        //最大值
+        hql = "select  max(id) from Student";
+        Integer max = session.createQuery(hql, Integer.class).uniqueResult();
+        System.out.println(max); //最大值
+
+
+        //最小值
+        hql = "select  min(age) from Student";
+        Integer min = session.createQuery(hql, Integer.class).uniqueResult();
+        System.out.println(min);
+
+        //平均值
+        hql = "select  avg(score) from Student";
+        Double avg = session.createQuery(hql, Double.class).uniqueResult();
+        System.out.println(avg);
+
+        //求和
+        hql = "select  sum (score) from Student";
+        Long sum = session.createQuery(hql, Long.class).uniqueResult();
+        System.out.println(sum);
+
+
+        transaction.commit();
+    }
+
+    @Test
+    public void test8() {
+        //分组查询
+        Transaction transaction = session.beginTransaction();
+        String hql = " from Student group by  age";
+        List<Student> list = session.createQuery(hql, Student.class).list();
+        list.forEach(System.out::println);
+
+
+
+        hql="select  age from Student group by  age";
+        List<Integer> ageList = session.createQuery(hql, Integer.class).list();
+        ageList.forEach(System.out::println);
 
         transaction.commit();
     }
--- src/main/java/com/ksea/hibernate/query/qbc/HibernateCriteriaQuery.java
+++ src/main/java/com/ksea/hibernate/query/qbc/HibernateCriteriaQuery.java
@@ -7,6 +7,7 @@ import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.criterion.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -34,6 +35,109 @@ public class HibernateCriteriaQuery {
         //这里在5.2版本之后，Criteria已经过时，Hibernate更是用JPA的方式来获取
         List list = session.createCriteria(Student.class).list();
         System.out.println(list);
+        transaction.commit();
+    }
+
+    @Test
+    public void test2() {
+        //QCB 排序查询
+        Transaction transaction = session.beginTransaction();
+        List list = session.createCriteria(Student.class).addOrder(Order.desc("age")).list();
+        list.forEach(System.out::println);
+        transaction.commit();
+    }
+
+    @Test
+    public void test3() {
+        //QCB 动态绑定参数，执行数据查询
+        Transaction transaction = session.beginTransaction();
+        List list = session.createCriteria(Student.class)
+                .add(Restrictions.ge("age", 36)) // age>=36
+                .add(Restrictions.le("score", 88)) //score<=88
+                .list();
+        list.forEach(System.out::println);
+        transaction.commit();
+    }
+
+    @Test
+    public void test4() {
+        //QCB 分页查询
+        Transaction transaction = session.beginTransaction();
+        List list = session.createCriteria(Student.class)
+                .setFirstResult((2 - 1) * 5) //startIndex=(startIndex-1)*pageSize
+                .setMaxResults(5) //pageSize
+                .list();
+        list.forEach(System.out::println);
+        transaction.commit();
+    }
+
+    @Test
+    public void test5() {
+        //QCB like 模糊查询
+        Transaction transaction = session.beginTransaction();
+        List list = session.createCriteria(Student.class)
+                .add(Restrictions.like("name", "%3%"))
+                .list();
+        list.forEach(System.out::println);
+        transaction.commit();
+    }
+
+    @Test
+    public void test6() {
+        //QCB uniue唯一性查询
+        Transaction transaction = session.beginTransaction();
+        Student student = (Student) session.createCriteria(Student.class)
+                .add(Restrictions.eq("id", 3))
+                .uniqueResult();
+        System.out.println(student);
+        transaction.commit();
+    }
+
+
+    @Test
+    public void test7() {
+        //QCB 聚合函数查询
+        Transaction transaction = session.beginTransaction();
+        Long rowCount = (Long) session.createCriteria(Student.class)
+                .setProjection(Projections.rowCount())
+                .uniqueResult();
+        System.out.println(rowCount); //统计总行数 :20
+
+
+        Long count = (Long) session.createCriteria(Student.class).setProjection(Projections.count("id")).uniqueResult();
+        System.out.println(count); //统计总行数:20
+
+
+        //统计id最大值
+        Integer max = (Integer) session.createCriteria(Student.class).setProjection(Projections.max("id")).uniqueResult();
+        System.out.println(max);
+
+        //统计age最小值
+        Integer min = (Integer) session.createCriteria(Student.class).setProjection(Projections.min("age")).uniqueResult();
+        System.out.println(min);
+
+        //统计score平均值
+        Double avg = (Double) session.createCriteria(Student.class).setProjection(Projections.avg("score")).uniqueResult();
+        System.out.println(avg);
+
+        //统计总分数
+        Long sum = (Long) session.createCriteria(Student.class).setProjection(Projections.sum("score")).uniqueResult();
+        System.out.println(sum);
+
+
+        transaction.commit();
+    }
+
+    @Test
+    public void test8() {
+        //分组查询
+        Transaction transaction = session.beginTransaction();
+        List list = session.createCriteria(Student.class)
+                .setProjection(Projections.groupProperty("age"))
+                .list();
+
+        list.forEach(System.out::println);
+
         transaction.commit();
     }
 }
