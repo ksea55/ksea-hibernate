c2891f89edc924a0a5bae8508f82061adbf55064
ksea55 <wangyang198118@126.com>
2017-06-28 10:55:52 +0800

    QBC Criteria查询 这种方式在Hibernate5.2之后的版本已经过时了

--- src/main/java/com/ksea/hibernate/query/qbc/HibernateCriteriaQuery.java
+++ src/main/java/com/ksea/hibernate/query/qbc/HibernateCriteriaQuery.java
@@ -1,11 +1,16 @@
 package com.ksea.hibernate.query.qbc;
 
+import com.ksea.hibernate.bo.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by mexican on 2017/6/27.
@@ -20,5 +25,15 @@ public class HibernateCriteriaQuery {
         SessionFactory sessionFactory = new MetadataSources().buildMetadata(serviceRegistry).buildSessionFactory();
         session = sessionFactory.getCurrentSession();
 
+    }
+
+    @Test
+    public void test1() {
+
+        Transaction transaction = session.beginTransaction();
+        //这里在5.2版本之后，Criteria已经过时，Hibernate更是用JPA的方式来获取
+        List list = session.createCriteria(Student.class).list();
+        System.out.println(list);
+        transaction.commit();
     }
 }
